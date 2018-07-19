f9861e0d7dd8b2df649106cfd8d92443acca0cb3
Adam Basiak <basiakus@gmail.com>
2018-07-19 21:02:40 +0200

    create repo

---
+++ js/script.js
@@ -0,0 +1,58 @@
+const canvasElement = document.getElementById('canvasElement');
+const canvas = canvasElement.getContext('2d');
+let isDrawing = false;
+let drawDirection = true;
+let lastOffsetY = 0;
+let lastOffsetX = 0;
+let hslColor = 0;
+
+canvasElement.width = window.innerWidth;
+canvasElement.heigh = window.innerHeight;
+
+window.innerWidth > 800 ? canvas.font = "4em Comic Sans MS" : canvas.font = "2.5em Comic Sans MS"  ;
+
+canvas.fillStyle = 'rgba(128,0,128 , .2)';
+canvas.textAlign = "center";
+canvas.fillText('Press or touch',canvasElement.width / 2, canvasElement.height / 2.4);
+canvas.fillText('and',canvasElement.width / 2, canvasElement.height / 2);
+canvas.fillText('Feel free',canvasElement.width / 2, canvasElement.height / 1.7);
+
+canvas.strokeStyle = 'purple';
+canvas.lineCap = "round";
+canvas.lineJoin = "round";
+canvas.lineWidth = 50;
+
+function startFromClick(e) {
+	isDrawing = true;
+	lastOffsetY = e.offsetY;
+	lastOffsetX = e.offsetX;
+}
+function draw(e) {
+	if(!isDrawing) return;
+	console.log(hslColor);
+	canvas.strokeStyle = `hsl(${hslColor}, 100%, 50%)`;
+	canvas.beginPath();
+	canvas.moveTo(lastOffsetX, lastOffsetY);
+	canvas.lineTo(e.offsetX, e.offsetY);
+	canvas.stroke();
+	lastOffsetY = e.offsetY;
+	lastOffsetX = e.offsetX;
+	hslColor++;
+
+	if(hslColor > 360) hslColor = 0;
+
+	if(canvas.lineWidth >= 50 || canvas.lineWidth <= 1) {
+		drawDirection = !drawDirection;
+	}
+
+	drawDirection ? canvas.lineWidth++ : canvas.lineWidth--;
+	
+}
+
+canvasElement.addEventListener('mousedown', startFromClick);
+canvasElement.addEventListener('touchstart', startFromClick);
+canvasElement.addEventListener('mousemove', draw);
+canvasElement.addEventListener('touchmove', draw);
+canvasElement.addEventListener('mouseout', () => isDrawing = false);
+canvasElement.addEventListener('mouseup', () => isDrawing = false);
+canvasElement.addEventListener('touchend', () => isDrawing = false);
